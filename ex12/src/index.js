e855500b0f00b47dbd467cb6adbc29be9fdae88e
desmosedici54 <mirrberr@gmail.com>
2022-04-01 16:48:52 +0200

    Adding the ex12

---
+++ ex12/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+
+    var lastName = "Tesla";
+    var thirdLetterOfLastName;
+    thirdLetterOfLastName = lastName[0,1,2];
+   
+    return thirdLetterOfLastName;
+    }  
+console.log(main());    
+module.exports = main;   
