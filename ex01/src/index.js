e6ce79f562e05b0200d21dfef23bffede3e0c701
desmosedici54 <mirrberr@gmail.com>
2022-04-01 12:38:07 +0200

    Adding the ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+
+    var myFirstName = "Mirza";
+    var myLastName = "Berbic";
+    var myHomeTown = "Banovici";
+    
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+        
+console.log(main());    
+module.exports = main;
