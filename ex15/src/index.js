1d4eb0843c649e3a135a1004d4ec62daa16509a0
desmosedici54 <mirrberr@gmail.com>
2022-04-01 17:51:38 +0200

    Adding the ex15

---
+++ ex15/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = "Black " + myAdjective + " dangerous " + myNoun + " can " + myVerb + " so " + myAdverb + "."; 
+
+    return wordBlanks;
+    }  
+console.log(main());    
+module.exports = main;  
