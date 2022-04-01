d138ffd4f285c90431a4c77e80c5aa8b25935a80
desmosedici54 <mirrberr@gmail.com>
2022-04-01 15:20:52 +0200

    Adding the ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+
+    var myName = "Mirza";
+    var myString = "My name is " + myName + " and I am well!";
+    
+    return myString;
+    }  
+console.log(main());    
+module.exports = main;  
